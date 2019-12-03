dd9158af269313891bc282bc9142825fd7c2f40b
Gareth Somerville <gareth.somerville@infinityworks.com>
2019-12-03 01:03:57 +0000

    List comprehension for input

--- day1.py
+++ day1.py
@@ -12,11 +12,10 @@ def get_fuel_requirement_recursive(module_weight: int, running_total: int):
     return get_fuel_requirement_recursive(fuel, running_total + fuel)
 
 
-file = open("day1_input.txt", "r")
+weights = [int(x) for x in open("day1_input.txt", "r").readlines()]
 total = 0
 total_with_fuel = 0
-for line in file.readlines():
-    weight = int(line)
+for weight in weights:
     total += get_fuel_requirement(weight)
     total_with_fuel += get_fuel_requirement_recursive(weight, 0)
 
