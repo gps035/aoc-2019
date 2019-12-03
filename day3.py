4b45a7d72b86842f4580bf452b8df68fae5a2510
Gareth Somerville <gareth.somerville@infinityworks.com>
2019-12-03 23:37:56 +0000

    MOAR PYTHONIC

--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
-from typing import List, Tuple
+from typing import List
 
 wire_paths = [x.split(",") for x in open("day3_input.txt", "r").readlines()]
 
-movement_amounts = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
+step_vectors = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 
 def create_path(instructions: List[str]):
@@ -10,34 +10,29 @@ def create_path(instructions: List[str]):
     steps = 0
     for movement in instructions:
         direction = movement[0]
-        movement_amount: Tuple[int, int] = movement_amounts[direction]
         distance = int(movement[1:])
+        step_vector = step_vectors[direction]
         for _ in range(distance):
-            current_pos = (
-                current_pos[0] + movement_amount[0],
-                current_pos[1] + movement_amount[1]
-            )
+            new_x = current_pos[0] + step_vector[0]
+            new_y = current_pos[1] + step_vector[1]
+            current_pos = (new_x, new_y)
             steps += 1
             yield current_pos, steps
 
 
 # It is assumed that the path of a single wire does not overlap itself
-paths = []
+paths = [list(create_path(x)) for x in wire_paths]
+path_points = [set(point for point, steps in path) for path in paths]
+wires_crossed = set.intersection(*path_points)
+
 distances = {}
+for x in paths:
+    for point, steps in x:
+        if point not in wires_crossed:
+            continue
+        total = distances.get(point, 0)
+        distances[point] = total + steps
 
-for x in wire_paths:
-    seen = set()
-    for point, steps in create_path(x):
-        if point not in distances.keys():
-            distances[point] = 0
-        seen.add(point)
-        distances[point] += steps
-    paths.append(seen)
+print(f"Part 1: {min([abs(i[0]) + abs(i[1]) for i in wires_crossed])}")
 
-intersections = paths[0]
-for x in paths[1:]:
-    intersections = intersections.intersection(x)
-
-print(f"Part 1: {min([abs(i[0]) + abs(i[1]) for i in intersections])}")
-
-print(f"Part 2: {min([distances[x] for x in intersections])}")
+print(f"Part 2: {min([distances[x] for x in wires_crossed])}")
