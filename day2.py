9961613b2a54c17731c7e7ab4dbd0d974ee78699
Gareth Somerville <gareth.somerville@infinityworks.com>
2019-12-05 08:28:50 +0000

    Move intcode computer to a new file

--- day2.py
+++ day2.py
@@ -1,34 +1,6 @@
 from typing import List
 
-
-def get_value(program: List[int], index: int):
-    return program[program[index]]
-
-
-def set_value(program: List[int], index: int, value: int):
-    program[program[index]] = value
-
-
-def run_program(program: List[int], noun: int, verb: int):
-    program = program.copy()
-    program[1] = noun
-    program[2] = verb
-    pc = 0
-
-    while True:
-        if program[pc] == 99:
-            break
-        elif program[pc] == 1:
-            result = get_value(program, pc + 1) + get_value(program, pc + 2)
-            set_value(program, pc + 3, result)
-            pc = pc + 4
-            continue
-        elif program[pc] == 2:
-            result = get_value(program, pc + 1) * get_value(program, pc + 2)
-            set_value(program, pc + 3, result)
-            pc = pc + 4
-
-    return program[0]
+from intcode_computer import run_program
 
 
 def find_noun_and_verb(expected_result: int, program: List[int]):
